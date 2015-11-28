f7745f2b53073d05a243be702e7ed654f4a3ddc4
NareshGoudJilla <Nareshjilla5@gmail.com>
2015-11-28 15:02:11 +0530

    Example

---
+++ Student.rb
@@ -0,0 +1,15 @@
+module Student
+  
+  
+  COLLEGE = "vrec"
+  COMPANY = "devahuthi"
+  FIRSTDAY = "Sunday"
+  ID = "1232"
+  NAME = "Naresh"
+  def Student.COMPANY
+    puts "Company Name is #{COMPANY}"
+  end
+  def Student.NAME
+    print "Student name is #{NAME}\n"
+  end
+end
