83a4193a7ad25e58934771fe25da2a5520361a04
NareshGoudJilla <Nareshjilla5@gmail.com>
2015-11-28 14:58:58 +0530

    Example

---
+++ Sample3.rb
@@ -0,0 +1,7 @@
+def method (*args)
+  puts "allow multiple #{args.length}"
+  for i in 0..args.length
+    puts "values are #{args[i]}"
+    end
+    end
+    method "Naresh","Avinash","chaitanya"  
