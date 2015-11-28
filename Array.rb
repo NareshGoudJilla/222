ae1064ffbcd6d60cba3189e150a01d55fdd8bed0
NareshGoudJilla <Nareshjilla5@gmail.com>
2015-11-28 13:56:33 +0530

    Hellow

---
+++ Array.rb
@@ -0,0 +1,11 @@
+a=[21,10,8,4]
+b=[12,15,18]
+#print (a+b).join()
+#print a[0]
+#print (a+b).sort, "\n"
+#a << 57 << 9 << 'phil'
+#print "A: ", a.join(' '), "\n"
+#b << 'vijaya' << 'Naveena' << 21 <<25
+#print "B= ",b.join(" "),"\n"
+b << 21
+print b
