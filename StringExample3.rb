6cab4277b2b9c27c8aa175bfb8d4e6c4817df449
NareshGoudJilla <Nareshjilla5@gmail.com>
2015-11-28 15:01:39 +0530

    Example

---
+++ StringExample3.rb
@@ -0,0 +1,7 @@
+n="Perseption may not be the true but it is reality...\n"
+#print "String= \n"+n;
+#print n.reverse\n;
+#print n.index("not");
+
+#print "[" + n[2,5] + "] [" + n[6..15] + "]\n"
+print n[10,20]
