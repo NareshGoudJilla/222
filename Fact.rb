b0518a646c91f10387456c035849b9fe25a3b9a2
NareshGoudJilla <Nareshjilla5@gmail.com>
2015-11-28 14:12:37 +0530

    Fact

---
+++ Fact.rb
@@ -0,0 +1,15 @@
+$fact = 1
+$i = 5
+while $i != 0 do
+$fact = $fact * $i
+$i-=1
+end
+puts("Result= #$fact" )
+
+#$i = 0
+#$num = 5
+
+#while $i < $num  do
+#   puts("Inside the loop i = #$i" )
+ #  $i +=1
+#end
