fac2b88008698dda26fea4fb48059d5a5bb3efa9
naresh <naresh@bitstack-vindhya>
2015-12-13 11:58:24 +0530

    Ruby Project

---
+++ RubyProject/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyProject::Application.config.secret_key_base = '371a46bce7ca9725544fcbecf36c0057e1fb0fe2ca50a35f0d294601536ed7ea3c25d68c14685152c0258e3fe6d4ba3cd6658d00345a173dcc48199445e65e34'
